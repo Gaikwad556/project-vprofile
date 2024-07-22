2e0e764234e490115a86c782b92eaf548665ae57
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-22 12:54:18 +0530

    update

--- terraform/docker.sh
+++ terraform/docker.sh
@@ -13,4 +13,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo apt install docker-compose -y
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
